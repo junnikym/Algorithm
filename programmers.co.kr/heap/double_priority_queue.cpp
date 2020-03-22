7ac42dc9c4d8b0c3b69eb087abfd32740a4a9379
junnikym <junnikym@gmail.com>
2020-03-22 23:55:46 +0900

    add new test
    programmers.co.kr - heap - double_priority_queue

---
+++ programmers.co.kr/heap/double_priority_queue.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+template <bool greater>
+bool comp(int* lhs, int* rhs) {
+    
+    
+    return greater ? (*lhs < rhs) : (*)
+}
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    priority_queue<int*, vector<int*>, comp>
+    
+    for(auto it : operations) {
+        if(it[0] == 'i'){
+            
+        }
+        else {
+            if(it[3] == 1) {    // delete max value
+                
+            }
+            else {              // delete min value
+                
+            }
+        }
+    }
+    
+    return answer;
+}
