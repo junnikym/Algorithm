22d1ff84287dc988d4ab978825b3de2a50b796fb
junnikym <junnikym@gmail.com>
2020-03-23 23:55:32 +0900

    Update double_priority_queue.cpp
    not solved

--- programmers.co.kr/heap/double_priority_queue.cpp
+++ programmers.co.kr/heap/double_priority_queue.cpp
@@ -1,33 +1,54 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
+#include <deque>
+
+#include <iostream>
 
 using namespace std;
 
-template <bool greater>
-bool comp(int* lhs, int* rhs) {
-    
-    
-    return greater ? (*lhs < rhs) : (*)
-}
-
 vector<int> solution(vector<string> operations) {
     vector<int> answer;
-    priority_queue<int*, vector<int*>, comp>
+    deque<int> dq;
+    int num = 0;
     
-    for(auto it : operations) {
-        if(it[0] == 'i'){
+    dq.push_back( stoi( (*operations.begin()).substr(2) ) );
+    for( auto it = operations.begin()+1; it != operations.end(); it++ ) {
+        if( (*it)[0] == 'I' ) {
+            num = stoi( (*it).substr(2) );
             
+            if( num < *dq.begin() ) 
+                dq.push_front(num);
+            
+            else if( !dq.empty() && num > *(dq.end()-1) ) 
+                dq.push_back(num);
+            
+            else {
+                for(auto dq_it = dq.begin(); dq_it != dq.end(); dq_it++) {
+                    if( num < *dq_it) {
+                        dq.insert(dq_it, num);
+                        break;
+                    }
+                }
+            }
         }
+            
         else {
-            if(it[3] == 1) {    // delete max value
-                
-            }
-            else {              // delete min value
-                
+            if( !dq.empty() ) {
+                if( (*it)[3] == 1 )     // delete max value
+                    dq.pop_back();
+                else                    // delete min value
+                    dq.pop_front();
             }
         }
+    }
+    
+    if(dq.empty()) 
+        answer = {0, 0};
+    else
+        answer = {dq.back(), dq.front()};
+    
+    for(auto dq_it : dq) {
+        cout << dq_it << ", ";
     }
     
     return answer;
