7dd4ec3378f388946a7f61d6bd3fa275e6dbe1ed
junnikym <junnikym@gmail.com>
2020-07-29 19:02:39 +0900

    complate DFS_BFS -> target number

--- programmers.co.kr/DFS_BFS/target_number.cpp
+++ programmers.co.kr/DFS_BFS/target_number.cpp
@@ -1,27 +1,33 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
+int dfs( int pre, int index, vector<int>& numbers, const int& target ) {
+    int answer = 0;
+    
+    if(index == numbers.size()-1) {
+        if( pre + numbers[index] == target )
+            answer++;
+
+        if( pre - numbers[index] == target)
+            answer++;
+
+        return answer;
+    }
+    else {
+        answer += dfs( pre+numbers[index], index+1, numbers, target);
+        answer += dfs( pre-numbers[index], index+1, numbers, target);
+    }
+
+    return answer;
+}
+
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    vector<bool> visited;
-    queue<int> visited_index;
 
-    // ! TODO : Initialize visited vector
+    answer += dfs(numbers[0], 1, numbers, target);
+    answer += dfs(-numbers[0], 1, numbers, target);
 
-    // Using BFS Algorithm
-    while() {
-        for(int i = 0; i < numbers.size(); i++) {
-            if(visited[i] == false) {
-                visited_index.push(i);
-                visited[i] == true;
-            }
-
-            break;
-        }
-    }
-    
     return answer;
 }
