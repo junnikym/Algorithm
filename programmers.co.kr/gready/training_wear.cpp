55aad0efb7b07150d14c1898c768babfbfb344a0
junnikym <junnikym@gmail.com>
2021-09-06 22:29:00 +0900

    프로그래머스 - 탐욕법 - 체육복 문제

---
+++ programmers.co.kr/gready/training_wear.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+using vec_iter = vector<int>::iterator;
+
+int solution(int n, vector<int> lost, vector<int> reserve)
+{
+	int answer = lost.size();
+	bool is_erased = false;
+
+	sort(lost.begin(), lost.end());
+	sort(reserve.begin(), reserve.end());
+
+	for (vec_iter l_it = lost.begin(); l_it != lost.end();)
+	{
+		for (vec_iter r_it = reserve.begin(); r_it != reserve.end(); r_it++)
+		{
+
+			if (*l_it == *r_it)
+			{
+				l_it = lost.erase(l_it);
+				reserve.erase(r_it);
+				is_erased = true;
+				break;
+			}
+		}
+
+		if (!is_erased)
+			++l_it;
+
+		is_erased = false;
+	}
+
+	for (vec_iter l_it = lost.begin(); l_it != lost.end();)
+	{
+		for (vec_iter r_it = reserve.begin(); r_it != reserve.end(); r_it++)
+		{
+
+			if (*l_it - 1 == *r_it || *l_it + 1 == *r_it)
+			{
+				l_it = lost.erase(l_it);
+				reserve.erase(r_it);
+				is_erased = true;
+				break;
+			}
+		}
+
+		if (!is_erased)
+			++l_it;
+
+		is_erased = false;
+	}
+
+	answer = n - lost.size();
+
+	return answer;
+}
+
+// ⠄⠄⠄⠄⠄⠄⣀⣀⣀⣤⣶⣿⣿⣶⣶⣶⣤⣄⣠⣴⣶⣿⣿⣿⣿⣶⣦⣄⠄⠄ 
+// ⠄⠄⣠⣴⣾⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦ 
+// ⢠⠾⣋⣭⣄⡀⠄⠄⠈⠙⠻⣿⣿⡿⠛⠋⠉⠉⠉⠙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿ 
+// ⡎⣾⡟⢻⣿⣷⠄⠄⠄⠄⠄⡼⣡⣾⣿⣿⣦⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⣿⣿ 
+// ⡇⢿⣷⣾⣿⠟⠄⠄⠄⠄⢰⠁⣿⣇⣸⣿⣿⠄⠄⠄⠄⠄⠄⠄⣠⣼⣿⣿⣿⣿ 
+// ⢸⣦⣭⣭⣄⣤⣤⣤⣴⣶⣿⣧⡘⠻⠛⠛⠁⠄⠄⠄⠄⣀⣴⣿⣿⣿⣿⣿⣿⣿ 
+// ⠄⢉⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
